e68690971cfc3c4c892658ac96892a71e70c0475
Akmalganteng10 <ycantent@gmail.com>
2024-10-05 14:28:26 +0900

    Add files via upload

---
+++ Akmaljidin.py
@@ -0,0 +1,13 @@
+x = int(input("masukan jumlah boneka:-"))
+jika x <=12:
+     print("harga per boneka Rp 20000")
+     print("jumlah", x*20000)
+elif x <=24:
+     print("harga per boneka Rp 19500")
+     print("jumlah", x*19500)
+elif x <=50:
+     print("harga per boneka Rp 18000")
+     print("jumlah", x*18000)
+else
+     print("harga per boneka Rp 17000")
+     print("jumlah", x*17000)
